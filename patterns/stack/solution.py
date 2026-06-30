4ca9f545ac0b20c68d188ffb8b6a5ac72c835106
Dilara Dinç <dilara.dinc.d@gmail.com>
2026-06-30 10:47:26 +0300

    add function to validate parentheses in a string

---
+++ patterns/stack/solution.py
@@ -0,0 +1,16 @@
+def is_valid(s):
+    stack = []
+    pairs = {')': '(', ']': '[', '}': '{'}
+
+    for char in s:
+        if char in "([{":
+            stack.append(char)
+        else:
+            if not stack or stack[-1] != pairs[char]:
+                return False
+            stack.pop()
+
+    return len(stack) == 0
+
+
+print(is_valid("()[]{}"))
