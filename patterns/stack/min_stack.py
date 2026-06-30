68efd8cafcce7625217a67b5bf984899355dd432
Dilara Dinç <dilara.dinc.d@gmail.com>
2026-06-30 11:04:22 +0300

    add MinStack implementation with min retrieval

---
+++ patterns/stack/min_stack.py
@@ -0,0 +1,16 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self):
+        if self.stack.pop() == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def get_min(self):
+        return self.min_stack[-1]
