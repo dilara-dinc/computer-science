b5f2113436be8ccdf0534b2c7332809add586b42
Dilara Dinç <dilara.dinc.d@gmail.com>
2026-06-30 13:24:19 +0300

    add function to rearrange zeros in an array

---
+++ algorithms/problems/arrays/move_zeros.py
@@ -0,0 +1,12 @@
+def move_zeros(nums):
+    slow = 0
+
+    for fast in range(len(nums)):
+        if nums[fast] != 0:
+            nums[slow], nums[fast] = nums[fast], nums[slow]
+            slow += 1
+
+    return nums
+
+
+print(move_zeros([0, 1, 0, 3, 12]))
